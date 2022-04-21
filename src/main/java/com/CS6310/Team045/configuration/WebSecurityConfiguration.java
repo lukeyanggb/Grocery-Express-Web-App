a3d5a72018351b1e743f6c2197dc597386c28bc1
Yang Lu <lukeyangcn@gmail.com>
2022-04-21 01:31:34 -0400

    add spring security for authentification

---
+++ src/main/java/com/CS6310/Team045/configuration/WebSecurityConfiguration.java
@@ -0,0 +1,43 @@
+package com.CS6310.Team045.configuration;
+
+import com.CS6310.Team045.services.AuthUserDetailsService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+@Configuration
+@EnableWebSecurity
+public class WebSecurityConfiguration extends WebSecurityConfigurerAdapter {
+    @Autowired
+    private AuthUserDetailsService authUserDetailsService;
+    @Bean
+    AuthenticationProvider authenticationProvider(){
+        DaoAuthenticationProvider provider = new DaoAuthenticationProvider();
+        provider.setUserDetailsService(authUserDetailsService);
+        provider.setPasswordEncoder(new BCryptPasswordEncoder());
+        return provider;
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests()
+                .antMatchers("/")
+                .permitAll()
+                .antMatchers("/user")
+                .hasAnyAuthority("USER", "ADMIN")
+                .antMatchers("/admin")
+                .hasAuthority("ADMIN")
+                .anyRequest()
+                .authenticated()
+                .and()
+                .formLogin()
+                .and()
+                .httpBasic();
+    }
+}
